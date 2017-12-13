31eee25cc8d18a0108d1718850d90d17ae6639f8
ArielBerkovich <34457341+ArielBerkovich@users.noreply.github.com>
2017-12-13 14:16:13 +0200

    Refactored main code
    todo: arguments need to come from a .config file

--- src/prototype/Main.java
+++ src/prototype/Main.java
@@ -1,31 +1,67 @@
 package prototype;
 	
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import client.Client;
 import javafx.application.Application;
 import javafx.application.Platform;
-import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
-import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
 
-import java.io.IOException;
-import java.util.List;
+public class Main extends Application 
+{
+	
+	//vars:
+			final public static int DEFAULT_PORT = 5555;
+			private Client client = null;
 
-public class Main extends Application {
-	
-	final public static int DEFAULT_PORT = 5555;
-	private Client client = null;
-	
-	// called on program start
+			
+//*************************************************************************************************
 	@Override
-	public void start(Stage primaryStage) {
-		try {
-			
-			// parse args and init clinet
-			parseArgs();
-			
-			if (client != null)
-			{			
+	public void start(Stage primaryStage) //--> this function is called on program start
+	{
+		try
+		{
+			initClient();
+			openNewClientGui(primaryStage);
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+			//exit program:
+			primaryStage.close();
+			Platform.exit();
+		}
+	}
+//*************************************************************************************************	
+	
+	
+//----------------------------------------------------------------------
+	
+	private void initClient() throws NullPointerException
+	{
+		ArrayList<Object> args = parseArgs();
+		
+		String host = (String) args.get(0);
+		int port = (int) args.get(1);
+		
+				try
+				{
+					client = new Client(host, port);
+				}
+				catch(IOException e)
+				{
+					System.out.println("Failed to connect to "+host+":"+port);
+					throw new NullPointerException();
+				}
+	}
+//----------------------------------------------------------------------
+	private void openNewClientGui(Stage primaryStage) throws IOException
+	{
 				FXMLLoader loader = new FXMLLoader(getClass().getResource("MainForm.fxml"));
 				BorderPane root = (BorderPane)loader.load();
 
@@ -41,19 +77,8 @@ public class Main extends Application {
 
 
 				primaryStage.show();
-			}
-			else
-			{
-				// Shutdown our app
-				primaryStage.close();
-				Platform.exit();
-			}
-		} 
-		catch(Exception e) {
-			e.printStackTrace();
-		}
 	}
-	
+//----------------------------------------------------------------------	
 	// called when program ends
 	@Override
 	public void stop()
@@ -62,17 +87,20 @@ public class Main extends Application {
 			client.quit();
 	}
 	
-	protected void parseArgs()
+	protected ArrayList<Object> parseArgs()
 	{
+		ArrayList<Object> output = new ArrayList<Object>();
 		String host = "";
 		int port = DEFAULT_PORT;
 		// read command line parameters
 		List<String> args = getParameters().getRaw();
 		
 		// set host(Server)
-		try {
+		try 
+		{
 			host = args.get(0);
-		}catch(IndexOutOfBoundsException e)
+		}
+		catch(IndexOutOfBoundsException e)
 		{
 			host = "localhost";
 		}
@@ -87,16 +115,10 @@ public class Main extends Application {
 				port = DEFAULT_PORT;
 			}
 		}
+		output.add(host);
+		output.add(port);
 		
-		// init client
-		try
-		{
-			client = new Client(host, port);
-		}
-		catch(IOException e)
-		{
-			System.out.println("Failed to connect to "+host+":"+port);
-		}
+		return output;
 	}
 	
 	public static void main(String[] args) {
