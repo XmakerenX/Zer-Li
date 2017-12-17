4a7aa64ca0b59d6ca16b209c2a63fa6875e8e3cf
Matan Keren <xmakerenx@gmail.com>
2017-12-17 12:39:06 +0200

    Added DBConnector class that will handle interaction with the DB

--- src/Server/ProtoTypeServer.java
+++ src/Server/ProtoTypeServer.java
@@ -16,7 +16,7 @@ import prototype.Product;
 public class ProtoTypeServer extends AbstractServer {
 
 	  final public static int DEFAULT_PORT = 5555;
-	  private Connection conn = null;
+	  private DBConnector db;
 	
 	  //-----------------------------------------------------------------
 	  // Constructors 
@@ -31,8 +31,8 @@ public class ProtoTypeServer extends AbstractServer {
 	  public ProtoTypeServer(int port, String username, String password) 
 	  {
 	    super(port);
-	    
-	    connectToDB(username, password);
+	    db = new DBConnector(username, password);
+	    //connectToDB(username, password);
 	  }
 
 	  //-----------------------------------------------------------------
@@ -60,21 +60,21 @@ public class ProtoTypeServer extends AbstractServer {
 			  {
 			  case "Product":
 			  {
-				  Statement stmt;
 				  ArrayList<Product> data = new ArrayList<Product>();
-
 				  try 
 				  {
-					  stmt = conn.createStatement();
-					  ResultSet rs = stmt.executeQuery("SELECT * FROM Product;");
-					  while(rs.next())
+					  ResultSet rs = db.selectTableData("*", "Product");
+					  if (rs != null)
 					  {
-						  // save the values in data						  
-						  data.add(new Product(rs.getInt(1), rs.getString(2), rs.getString(3)));
-					  } 
-					  rs.close();
-					  System.out.println("sending message");
-					  client.sendToClient(data);
+						  while(rs.next())
+						  {
+							  // save the values in data						  
+							  data.add(new Product(rs.getInt(1), rs.getString(2), rs.getString(3)));
+						  } 
+						  rs.close();
+						  System.out.println("sending message");
+						  client.sendToClient(data);
+					  }
 
 				  } catch (SQLException e) {e.printStackTrace();}
 				  catch (IOException e) {System.out.println("Could not send message to Client.");}
@@ -94,17 +94,13 @@ public class ProtoTypeServer extends AbstractServer {
 			  case "Product":
 			  {
 				  System.out.println(userInput);
-				  Statement stmt;
-				  try 
-				  {
-					  System.out.println("updating database");
-					  stmt = conn.createStatement();
-					  
-					  //stmt.executeUpdate("UPDATE Product SET ProductName=\""+userInput.get(3)+"\" ,ProductType=\""+userInput.get(4)+"\" WHERE ProductID="+userInput.get(2)+";");
-					  stmt.executeUpdate("UPDATE Product SET ProductID="+userInput.get(3)+" ,ProductName=\""+userInput.get(4)+"\" ,ProductType=\""+userInput.get(5)+"\" WHERE ProductID="+userInput.get(2)+";");
-					  
-				  } catch (SQLException e) {e.printStackTrace();}
-				  
+				  System.out.println("updating database");
+
+				  String productID = "ProductID="+userInput.get(3);
+				  String productName = "ProductName=\""+userInput.get(4)+"\"";
+				  String productType = "ProductType=\""+userInput.get(5)+"\"";
+				  String condition = "ProductID="+userInput.get(2); 
+				  db.executeUpdate("Product", productID + "," + productName + "," + productType, condition);
 			  }break;
 			  
 			  default:
@@ -138,47 +134,7 @@ public class ProtoTypeServer extends AbstractServer {
 	  {
 	    System.out.println("Server has stopped listening for connections.");
 	    // close connection to DB
-	    if (conn != null)
-	    {
-	    	try
-	    	{
-	    		conn.close();
-	    	}
-	    	catch (SQLException ex) {/* handle any errors */
-				System.out.println("SQLException: " + ex.getMessage());
-				System.out.println("SQLState: " + ex.getSQLState());
-				System.out.println("VendorError: " + ex.getErrorCode());
-			}
-	    }
-	  }
-	  
-	  /**
-	   * This method overrides create a connection to the local database
-	   * 
-	   * @param username The DB username
-	   * @param password The DB passowrd
-	   */
-	  public void connectToDB(String username, String password)
-	  {
-			// init driver
-			try {
-				Class.forName("com.mysql.jdbc.Driver").newInstance();
-			} catch (InstantiationException | IllegalAccessException | ClassNotFoundException ex) 
-			{
-				System.out.println("Failed to get jdbc Driver");
-				System.exit(0);
-			}
-
-			// init connection to database
-			try {
-				conn = DriverManager.getConnection("jdbc:mysql://localhost/prototype", username, password);
-
-			} catch (SQLException ex) {/* handle any errors */
-				System.out.println("SQLException: " + ex.getMessage());
-				System.out.println("SQLState: " + ex.getSQLState());
-				System.out.println("VendorError: " + ex.getErrorCode());
-				System.exit(0);
-			}
+	    db.closeConnection();
 	  }
 	  
 	  /**
