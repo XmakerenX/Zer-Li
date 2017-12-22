db4239f467a53ee8cea6ab4da4e70b2ece117dd0
dk198 <dk198@DESKTOP-QP97Q3U>
2017-12-22 16:49:33 +0200

    Working on customer satisfaction surveys

--- src/Surveys/CustomerSatisfactionSurvey.java
+++ src/Surveys/CustomerSatisfactionSurvey.java
@@ -3,14 +3,74 @@ package Surveys;
 import java.util.ArrayList;
 
 public class CustomerSatisfactionSurvey {
-	private String surveyID;
+	private String surveyName;
 	private String[] surveyQuestions;
-	private ArrayList<Integer[]> surveyAnswers;		//arraylist of int[6] arrays with survey answers
+	private ArrayList<CustomerSatisfactionSurveyResults> surveyResults;
 	private String surveyAnalysis;
-	public static int numberOfSurveys=0;
 	
-	public CustomerSatisfactionSurvey() {
+	public CustomerSatisfactionSurvey(String name, String[] questions) throws SurveyException
+	{
+		try 
+		{
+			setSurveyName(name);
+			setSurveyQuestions(questions);
+			setSurveyResults(null);
+		}
+		catch(SurveyException exception) 
+		{
+			throw exception;
+		}
 		
+	}
+
+	public String getSurveyName() {
+		return surveyName;
+	}
+
+	public void setSurveyName(String surveyName) {
+		if()				//------------------------------check whether there's one in the data base already
+		{
+			this.surveyName = surveyName;
+		}
+		else
+		{
+			throw new SurveyException("Survey with the same name already exists");
+		}
+	}
+
+	public String[] getSurveyQuestions() {
+		return surveyQuestions;
+	}
+
+	public void setSurveyQuestions(String[] surveyQuestions) throws SurveyException {
+		if(surveyQuestions.length!=6)
+		{
+			for(int i=0; i<6; i++)
+			{
+				this.surveyQuestions[i]=surveyQuestions[i];
+			}
+		}
+		else
+		{
+			throw new SurveyException("You must input 6 questions to create a survey.");
+		}
+		this.surveyQuestions = surveyQuestions;
+	}
+
+	public ArrayList<CustomerSatisfactionSurveyResults> getResults() {
+		return surveyResults;
+	}
+
+	public void setSurveyResults(ArrayList<CustomerSatisfactionSurveyResults> results) {
+		this.surveyResults = results;
+	}
+
+	public String getSurveyAnalysis() {
+		return surveyAnalysis;
+	}
+
+	public void setSurveyAnalysis(String surveyAnalysis) {
+		this.surveyAnalysis = surveyAnalysis;
 	}
 
 }
---
+++ src/Surveys/CustomerSatisfactionSurveyResults.java
@@ -0,0 +1,36 @@
+package Surveys;
+
+import java.sql.Date;
+
+public class CustomerSatisfactionSurveyResults {
+	private Date date;
+	private String[] answers;
+	
+	
+	public CustomerSatisfactionSurveyResults(String[] answers) {
+		setDate();//<---------------------------------get this one
+		setAnswers(answers);
+		
+	}
+
+
+	public Date getDate() {
+		return date;
+	}
+
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+
+	public String[] getAnswers() {
+		return answers;
+	}
+
+
+	public void setAnswers(String[] answers) {
+		this.answers = answers;
+	}
+
+}
