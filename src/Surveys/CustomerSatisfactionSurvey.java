1098fdd752bb4f88ff4664ea78b7b7ebc05fa39c
dk198 <dk198@DESKTOP-QP97Q3U>
2017-12-22 11:26:42 +0200

    Working on customer satisfaction survey

---
+++ src/Surveys/CustomerSatisfactionSurvey.java
@@ -0,0 +1,16 @@
+package Surveys;
+
+import java.util.ArrayList;
+
+public class CustomerSatisfactionSurvey {
+	private String surveyID;
+	private String[] surveyQuestions;
+	private ArrayList<Integer[]> surveyAnswers;		//arraylist of int[6] arrays with survey answers
+	private String surveyAnalysis;
+	public static int numberOfSurveys=0;
+	
+	public CustomerSatisfactionSurvey() {
+		
+	}
+
+}
---
+++ src/Surveys/CustomerSatisfactionSurveyController.java
@@ -0,0 +1,5 @@
+package Surveys;
+
+public class CustomerSatisfactionSurveyController {
+
+}
