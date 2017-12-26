f73e512a6e974cbccbbbd2f7d832f9743a01a3e5
dk198 <dk198@169.254.139.115>
2017-12-26 14:08:18 +0200

    Comments on my code

--- src/Surveys/CustomerSatisfactionSurveyController.java
+++ src/Surveys/CustomerSatisfactionSurveyController.java
@@ -1,11 +1,11 @@
-package Surveys;
-
-public class CustomerSatisfactionSurveyController {
-	
-	public boolean doesSurveyExist(String surveyName) { //<--------------------------check if a survey with such a name exists in the DB
-		
-		
-		return false;
-	}
-
-}
+//package Surveys;
+//
+//public class CustomerSatisfactionSurveyController {
+//	
+//	public boolean doesSurveyExist(String surveyName) { //<--------------------------check if a survey with such a name exists in the DB
+//		
+//		
+//		return false;
+//	}
+//
+//}
--- src/Surveys/SurveyException.java
+++ src/Surveys/SurveyException.java
@@ -1,8 +1,8 @@
-package Surveys;
-
-public class SurveyException extends Exception{
-	public SurveyException(String message) {
-		super(message);
-	}
-
-}
+//package Surveys;
+//
+//public class SurveyException extends Exception{
+//	public SurveyException(String message) {
+//		super(message);
+//	}
+//
+//}
