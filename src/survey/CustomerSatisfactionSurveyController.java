b002919804b9a4dc40059050232ace1fd47650b4
dk198 <dk198@DESKTOP-QP97Q3U>
2017-12-29 18:22:08 +0200

    Survey creation work in progress

--- src/survey/CustomerSatisfactionSurveyController.java
+++ src/survey/CustomerSatisfactionSurveyController.java
@@ -1,11 +1,51 @@
 package survey;
 
+import client.Client;
+import serverAPI.AddRequest;
+import serverAPI.CheckExistsRequest;
+import serverAPI.GetRequestByKey;
+import serverAPI.LoginRequest;
+import serverAPI.Response;
+import user.LoginException;
+import user.User;
+import user.User.UserException;
+
 public class CustomerSatisfactionSurveyController {
 	
-	public boolean doesSurveyExist(String surveyName) { //<--------------------------check if a survey with such a name exists in the DB
+	//===============================================================================================================
+	public static void getSurvey(String surveyName, Client client)
+	{
+		client.handleMessageFromClientUI(new GetRequestByKey("surveys", surveyName));
 		
-		
-		return false;
 	}
+	//===============================================================================================================
+	/**
+	 * checks whether the survey with such a name exists in the data base
+	 * @param surveyName
+	 * @param client
+	 */
+	public static void surveyCreation(String surveyName, String[] questions, Client client)
+	{		
+		try {
+			CustomerSatisfactionSurvey newSurvey = new CustomerSatisfactionSurvey(surveyName, questions);
+			client.handleMessageFromClientUI(new AddRequest("surveys", newSurvey));
+		} catch (SurveyException e) {
+			// TODO deal with error
+			// shouldn't get here!
+			e.printStackTrace();
+		}	
+	}
+	//===============================================================================================================
+//	public static void verifySurveyCreation (String surveyName, String[] questions) throws SurveyException
+//	{
+//		//checking if all the fields are alright and throwing exceptions if not so.
+//	}
+	
+	//===============================================================================================================
+	public static void doesSurveyExist(String surveyName, Client client)
+	{
+		client.handleMessageFromClientUI(new CheckExistsRequest("surveys", surveyName));
+	}
+
 
 }
--- src/survey/SurveyCreationGUI.java
+++ src/survey/SurveyCreationGUI.java
@@ -2,6 +2,7 @@ package survey;
 
 import java.util.ArrayList;
 
+import client.Client;
 import javafx.event.ActionEvent;
 
 /**
@@ -9,11 +10,20 @@ import javafx.event.ActionEvent;
  */
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
+import prototype.FormController;
+import serverAPI.Response;
+import user.User;
 
-public class SurveyCreationGUI {
-
+public class SurveyCreationGUI extends FormController{
+	
+	// holds the last replay we got from server
+	private Response response = null;
+		
     @FXML // fx:id="surveyNameTxtFld"
     private TextField surveyNameTxtFld; // Value injected by FXMLLoader
 
@@ -51,29 +61,111 @@ public class SurveyCreationGUI {
     void closeWindow(ActionEvent event) {
 
     }
-
+  //===============================================================================================================
     @FXML
     void createSurvey(ActionEvent event) {
-    	ArrayList<String> questions = null;
+    	String[] questions = null;
     	String surveyName = surveyNameTxtFld.getText();
-    	questions.add(question1TxtFld.getText());
-    	questions.add(question2TxtFld.getText());
-    	questions.add(question3TxtFld.getText());
-    	questions.add(question4TxtFld.getText());
-    	questions.add(question5TxtFld.getText());
-    	questions.add(question6TxtFld.getText());
-    	questions.add(question7TxtFld.getText());
-    	questions.add(question8TxtFld.getText());
-    	if(surveyName==null)
+    	int questionsFault = 0;
+    	int nameFault = 0;
+    	questions[0]=question1TxtFld.getText();
+    	questions[1]=question2TxtFld.getText();
+    	questions[2]=question3TxtFld.getText();
+    	questions[3]=question4TxtFld.getText();
+    	questions[4]=question5TxtFld.getText();
+    	questions[5]=question6TxtFld.getText();
+    	questions[6]=question7TxtFld.getText();
+    	questions[7]=question8TxtFld.getText();
+    	if(surveyName==null) nameFault=1;
+    	for(int i=0; i<8; i++)
     	{
-    		
+    		if(questions[i]==null)
+    		{
+    			questionsFault=1;
+    			break;
+    		}		
     	}
-    	if((questions.get(0) || questions.get(1) || questions.get(2)||questions.get(3) )==null)
+    	if(nameFault!=0 && questionsFault!=0)	//both survey name and 8 questions have been filled:
     	{
-    		
-    		
-    	}
+    		CustomerSatisfactionSurveyController.doesSurveyExist(surveyName, client);
+    		try
+        	{
+        		synchronized(this)
+        		{
+        			// wait for server response
+        			this.wait();
+        		}
+        	
+        		if (response == null)
+        			return;
+	        		
+	        	// show success 
+	        	if (response.getType() == Response.Type.SUCCESS)
+	        	{
+	        		// show failure  
+	        		Alert alert = new Alert(AlertType.ERROR, "Survey with the same name already exists.", ButtonType.OK);
+	        		alert.showAndWait();
+	        		// clear replay
+	        		response = null;
+	        	}
+	        	else
+	        	{
+	        		CustomerSatisfactionSurveyController.surveyCreation(surveyName, questions, client);
+	        		try
+	        		{
+	        			synchronized(this)
+	        			{
+	        				//wait for server response
+	        				this.wait();
+	        			}
+	        			if(response == null)
+	        				return;
+	        			
+	        			//success
+	        			if(response.getType() == Response.Type.SUCCESS)
+	        			{
+	        				Alert alert = new Alert(AlertType.CONFIRMATION, "Survey created successfully", ButtonType.OK);
+	    	        		alert.showAndWait();
+	    	        		// clear replay
+	    	        		response = null;
+	        			}
+	        			else
+	        			{
+	        				// show failure  
+	        	    		Alert alert = new Alert(AlertType.ERROR, (String)response.getMessage(), ButtonType.OK);
+	        	    		alert.showAndWait();
+	        	    		// clear replay
+	        	    		response = null;
+	        			}
+	        		}
+	        		catch(InterruptedException e) {}
 
+	        	}
+	        	
+	        	}catch(InterruptedException e) {}
+    	}
+    	else
+    	{
+    		if(nameFault==1) {
+    			// show failure  
+	    		Alert alert = new Alert(AlertType.ERROR, "A name is needed to create a survey.", ButtonType.OK);
+	    		alert.showAndWait();
+    		}
+    		else {
+    			// show failure  
+	    		Alert alert = new Alert(AlertType.ERROR, "You must input all eight(8) questions to create a survey.", ButtonType.OK);
+	    		alert.showAndWait();
+    		}
+    	}
+    	
+    	
+    	
     }
+  //===============================================================================================================
+	@Override
+	public void onSwitch(Client newClient) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
