8ad4cec802ee88d5ac2b4627e39a05297177c387
Matan Keren <xmakerenx@gmail.com>
2018-01-05 18:43:58 +0200

    Updated currentDB.sql with catalog tables

--- currentDB.sql
+++ currentDB.sql
@@ -42,6 +42,38 @@ INSERT INTO `CatalogProduct` VALUES (4,100,'panter.jpg'),(5,-1,'buttercup.jpg'),
 UNLOCK TABLES;
 
 --
+-- Table structure for table `Order`
+--
+
+DROP TABLE IF EXISTS `Order`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Order` (
+  `OrderID` int(11) NOT NULL AUTO_INCREMENT,
+  `OrderStatus` varchar(45) DEFAULT NULL,
+  `OrderPrice` float DEFAULT NULL,
+  `OrderRequiredDate` date DEFAULT NULL,
+  `OrderRequiredTime` varchar(45) DEFAULT NULL,
+  `OrderShipmentAddress` varchar(45) DEFAULT NULL,
+  `OrderReceiverName` varchar(45) DEFAULT NULL,
+  `OrderReceiverPhoneNumber` varchar(45) DEFAULT NULL,
+  `OrderPaymentMethod` varchar(45) DEFAULT NULL,
+  `OrderOriginStore` int(11) DEFAULT NULL,
+  PRIMARY KEY (`OrderID`)
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Order`
+--
+
+LOCK TABLES `Order` WRITE;
+/*!40000 ALTER TABLE `Order` DISABLE KEYS */;
+INSERT INTO `Order` VALUES (22,'NEW',378,'2018-01-12','14:00','Haifa','boby','048769834','CREDITCARD',0),(23,'NEW',94,'2018-01-18','20:15',NULL,NULL,NULL,'CREDITCARD',0);
+/*!40000 ALTER TABLE `Order` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `Product`
 --
 
@@ -70,33 +102,59 @@ INSERT INTO `Product` VALUES (1,'roses','Red',30,15,'Red'),(2,'Chrysanthemums','
 UNLOCK TABLES;
 
 --
--- Table structure for table `surveys`
+-- Table structure for table `ProductInOrder`
 --
 
-DROP TABLE IF EXISTS `surveys`;
+DROP TABLE IF EXISTS `ProductInOrder`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `surveys` (
-  `surveyName` varchar(200) NOT NULL,
-  `question1` varchar(200) DEFAULT NULL,
-  `question2` varchar(200) DEFAULT NULL,
-  `question3` varchar(200) DEFAULT NULL,
-  `question4` varchar(200) DEFAULT NULL,
-  `question5` varchar(200) DEFAULT NULL,
-  `question6` varchar(200) DEFAULT NULL,
-  `analysis` varchar(500) DEFAULT NULL,
-  PRIMARY KEY (`surveyName`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `ProductInOrder` (
+  `ProductID` int(11) NOT NULL,
+  `OrderID` int(11) NOT NULL,
+  `ProductGrettingCard` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`ProductID`,`OrderID`),
+  KEY `fk_ProductInOrder_2_idx` (`OrderID`),
+  CONSTRAINT `fk_ProductInOrder_1` FOREIGN KEY (`ProductID`) REFERENCES `Product` (`ProductID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_ProductInOrder_2` FOREIGN KEY (`OrderID`) REFERENCES `Order` (`OrderID`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `surveys`
+-- Dumping data for table `ProductInOrder`
 --
 
-LOCK TABLES `surveys` WRITE;
-/*!40000 ALTER TABLE `surveys` DISABLE KEYS */;
-INSERT INTO `surveys` VALUES ('12','1','1','1','1','1','1','1'),('123','Question','Question','Question','Question','Question','Question','Bla bla'),('12355','1','1','1','1','1','1',NULL),('new survey','1','1','1','1','1','1',NULL);
-/*!40000 ALTER TABLE `surveys` ENABLE KEYS */;
+LOCK TABLES `ProductInOrder` WRITE;
+/*!40000 ALTER TABLE `ProductInOrder` DISABLE KEYS */;
+INSERT INTO `ProductInOrder` VALUES (4,22,'Awesome Panger'),(5,22,'Hi Buttercup'),(5,23,''),(6,22,'');
+/*!40000 ALTER TABLE `ProductInOrder` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `User`
+--
+
+DROP TABLE IF EXISTS `User`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `User` (
+  `UserName` varchar(45) NOT NULL,
+  `UserPassword` varchar(45) DEFAULT NULL,
+  `UserPermission` varchar(45) DEFAULT NULL,
+  `personID` int(11) DEFAULT NULL,
+  `UserStatus` varchar(45) DEFAULT NULL,
+  `unsuccessfulTries` int(11) DEFAULT NULL,
+  PRIMARY KEY (`UserName`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `User`
+--
+
+LOCK TABLES `User` WRITE;
+/*!40000 ALTER TABLE `User` DISABLE KEYS */;
+INSERT INTO `User` VALUES ('admin','123456','SYSTEM_MANAGER',0,'REGULAR',0),('daniel','123456','CUSTOMER_SERVICE',123,'REGULAR',0),('jenia','123456','CUSTOMER_SERVICE_EXPERT',1111,'REGULAR',0),('matan','qwerty','STORE_WORKER',1,'REGULAR',0),('matan2','123456','CUSTOMER',305022949,'REGULAR',0);
+/*!40000 ALTER TABLE `User` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -131,31 +189,33 @@ INSERT INTO `customersatisfactionsurveyresults` VALUES (1,'new surve','2018-01-0
 UNLOCK TABLES;
 
 --
--- Table structure for table `User`
+-- Table structure for table `surveys`
 --
 
-DROP TABLE IF EXISTS `User`;
+DROP TABLE IF EXISTS `surveys`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `User` (
-  `UserName` varchar(45) NOT NULL,
-  `UserPassword` varchar(45) DEFAULT NULL,
-  `UserPermission` varchar(45) DEFAULT NULL,
-  `personID` int(11) DEFAULT NULL,
-  `UserStatus` varchar(45) DEFAULT NULL,
-  `unsuccessfulTries` int(11) DEFAULT NULL,
-  PRIMARY KEY (`UserName`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE `surveys` (
+  `surveyName` varchar(200) NOT NULL,
+  `question1` varchar(200) DEFAULT NULL,
+  `question2` varchar(200) DEFAULT NULL,
+  `question3` varchar(200) DEFAULT NULL,
+  `question4` varchar(200) DEFAULT NULL,
+  `question5` varchar(200) DEFAULT NULL,
+  `question6` varchar(200) DEFAULT NULL,
+  `analysis` varchar(500) DEFAULT NULL,
+  PRIMARY KEY (`surveyName`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `User`
+-- Dumping data for table `surveys`
 --
 
-LOCK TABLES `User` WRITE;
-/*!40000 ALTER TABLE `User` DISABLE KEYS */;
-INSERT INTO `User` VALUES ('admin','123456','SYSTEM_MANAGER',0,'REGULAR',0),('matan','qwerty','STORE_WORKER',1,'REGULAR',0),('matan2','123456','CUSTOMER',305022949,'REGULAR',0), ('daniel','123456','CUSTOMER_SERVICE',123,'REGULAR',0),('jenia','123456','CUSTOMER_SERVICE_EXPERT',1111,'REGULAR',0);
-/*!40000 ALTER TABLE `User` ENABLE KEYS */;
+LOCK TABLES `surveys` WRITE;
+/*!40000 ALTER TABLE `surveys` DISABLE KEYS */;
+INSERT INTO `surveys` VALUES ('12','1','1','1','1','1','1','1'),('123','Question','Question','Question','Question','Question','Question','Bla bla'),('12355','1','1','1','1','1','1',NULL),('new survey','1','1','1','1','1','1',NULL);
+/*!40000 ALTER TABLE `surveys` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -167,4 +227,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-12-29 15:23:58
+-- Dump completed on 2018-01-05 18:42:19
