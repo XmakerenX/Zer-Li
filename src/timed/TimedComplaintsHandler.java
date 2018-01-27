ff88cad8f8541b77e4d0f2a60a32463ef98a7eac
ariel <arielberkovich1@gmail.com>
2018-01-27 17:11:07 +0200

    complaints automated responses completed

--- src/timed/TimedComplaintsHandler.java
+++ src/timed/TimedComplaintsHandler.java
@@ -1,6 +1,7 @@
 package timed;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -8,7 +9,9 @@ import java.util.Calendar;
 import java.util.TimerTask;
 
 import Server.DBConnector;
+import customer.Customer;
 import order.OrderComplaint;
+import serverAPI.GetRequestByKey;
 import utils.EntityFactory;
 
 public class TimedComplaintsHandler extends TimerTask
@@ -23,8 +26,6 @@ public class TimedComplaintsHandler extends TimerTask
 	@Override
 	public void run() 
 	{
-		
-		
 		//Get result set of unhandled complaints
 		ResultSet rs = db.selectTableData("*", "ordercomplaint", "status = \"NEW\"");
 		
@@ -43,10 +44,13 @@ public class TimedComplaintsHandler extends TimerTask
 		int currMinutes = cal.getTime().getMinutes();
 		
 		String currentTime = currHours+":"+currMinutes;
-				
-		
+
 		for(OrderComplaint complaint : unhandledComplaints)
 		{
+			if (complaint.getComplaintCompensation() != 0.0)
+			{
+				continue;
+			}
 			LocalDate complaintDate = complaint.getComplaintDate();
 			LocalDate currentDate = LocalDate.now();
 			
@@ -57,13 +61,46 @@ public class TimedComplaintsHandler extends TimerTask
 			
 			 timePassed = timePassed ||  ((currentDate.isAfter(complaintDate)) &&					           
 					  						(currentTime.compareTo( complaintTime) !=-1)); 
-			
+	
+			System.out.println(timePassed);
 			if(timePassed) 
 			{
-				
+				ArrayList<String> keys = new ArrayList<String>();
+				keys.add(Long.toString(complaint.getCustomerID()));
+				keys.add(Integer.toString(complaint.getStoreID()));
+				try 
+				{
+					refundCustomer(keys,30);
+					db.executeUpdate("ordercomplaint", "givenCompensationAmount ="+30,"id = "+complaint.getComplaintID());
+				}
+				catch (SQLException e) 
+				{
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 			}
 			
 		}
 	}
 //===========================================================================================================
+	private void refundCustomer(ArrayList<String> customerKeys, float refundAmount) throws SQLException
+	  {
+		 String condition = "";
+		 condition = db.generateConditionForPrimayKey("Customers",customerKeys, condition);
+		 System.out.println(condition);
+		 ResultSet rs = db.selectTableData("*", "Customers", condition);
+		 
+		ArrayList<Customer> result = EntityFactory.loadCustomers(rs);
+		
+		Customer customer = result.get(0);
+		
+		float  newBalance = customer.getAccountBalance() +refundAmount;
+		db.executeUpdate("Customers", "accountBalance="+newBalance, 
+					  "PersonID="+customer.getID()+" AND StoreID="+customer.getStoreID() );
+		
+	
+	  }
+
+
+
 }
